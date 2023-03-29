c53bf07c48400a3f40131da1d93bc9ff36e8c01c
junho <yunjunhojj@gmail.com>
2023-03-29 21:51:53 +0900

    fix: main render 방식 수정

--- src/front/src/main.tsx
+++ src/front/src/main.tsx
@@ -1,15 +1,18 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import { BrowserRouter, useRoutes } from "react-router-dom";
-import App from "./App";
 import { ThemeProvider } from "styled-components";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 import theme from "./styles/theme";
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+
+root.render(
   <BrowserRouter>
     <ThemeProvider theme={theme}>
       <App />
     </ThemeProvider>
-  </BrowserRouter>,
-  document.getElementById("root")
+  </BrowserRouter>
 );
