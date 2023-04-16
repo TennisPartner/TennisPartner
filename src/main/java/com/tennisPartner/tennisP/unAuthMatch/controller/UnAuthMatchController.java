b21c4414962e3d03cd0c84acddd7f1eb91a98f1b
cosiw <cosiw@naver.com>
2023-04-16 17:19:33 +0900

    fix: 매칭이 실패했을 경우 내용 추가 사람에비해 코트수가 많을 경우 코트수가 감소되었다는 것을 알리기 위한 message 추가 issue50

--- src/main/java/com/tennisPartner/tennisP/unAuthMatch/controller/UnAuthMatchController.java
+++ src/main/java/com/tennisPartner/tennisP/unAuthMatch/controller/UnAuthMatchController.java
@@ -25,27 +25,32 @@ public class UnAuthMatchController {
     }
 
     @PostMapping()
-    public ResponseEntity<Map<String,List<List<Integer>>>> unAuthMatching(@RequestBody UnAuthMatchRequestDTO req){
+    public ResponseEntity<Map<String,Object>> unAuthMatching(@RequestBody UnAuthMatchRequestDTO req){
 
         int playerCnt = req.getPlayerCnt();
         int gameCnt = req.getGameCnt();
         int courtCnt = req.getCourtCnt();
+        String message = "";
         if(playerCnt < 4){
-            return new ResponseEntity("4명 이상 모여야 사용 가능합니다.",HttpStatus.BAD_REQUEST);
+            message = "4명 이상 모여야 사용 가능합니다. 인원수를 증가시키세요";
+            return new ResponseEntity(message,HttpStatus.BAD_REQUEST);
         }
         if(gameCnt * 4 < playerCnt){
-            return new ResponseEntity("1게임도 못하는 사람이 있습니다.", HttpStatus.BAD_REQUEST);
+            message = "1게임도 못하는 사람이 있습니다. 게임수를 증가시키거나, 인원수를 줄여주세요";
+            return new ResponseEntity(message, HttpStatus.BAD_REQUEST);
         }
 
         if(courtCnt * 4 > playerCnt){
             courtCnt = courtCnt - (courtCnt-playerCnt/4);
             req.setCourtCnt(courtCnt);
+            message = "코트에비해 사람이 적어 코트 수를 " + courtCnt + "로 감소하였습니다.";
         }
 
         List<List<Integer>> totalList = unAuthMatchService.unAuthMatching(req);
-        System.out.println(totalList);
-        Map<String, List<List<Integer>>> result = new HashMap<>();
+        Map<String, Object> result = new HashMap<>();
         result.put("gameList", totalList);
+        result.put("message", message);
+
 
         return new ResponseEntity<>(result, HttpStatus.OK);
 
