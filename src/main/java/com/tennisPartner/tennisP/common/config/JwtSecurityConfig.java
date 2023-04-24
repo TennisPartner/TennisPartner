41cb4739a1a765d426475082ccefbddc9dbc5c4a
Ockddak <czower123@gmail.com>
2023-04-24 12:30:40 +0900

    fix: JwtSecurityConfig 수정
    변경사항
    - header 추가
    
    #31

--- src/main/java/com/tennisPartner/tennisP/common/config/JwtSecurityConfig.java
+++ src/main/java/com/tennisPartner/tennisP/common/config/JwtSecurityConfig.java
@@ -77,6 +77,9 @@ public class JwtSecurityConfig {
                                 config.setAllowedMethods(
                                         List.of("*")
                                 );
+                                config.setAllowedHeaders(
+                                        List.of("*")
+                                );
                                 return config;
                             };
                             c.configurationSource(source);
