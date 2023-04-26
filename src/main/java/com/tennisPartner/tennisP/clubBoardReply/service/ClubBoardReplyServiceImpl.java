379e81ee87bbd2d25e40134e4d5f5294a9d04a8c
cosiw <cosiw@naver.com>
2023-04-26 16:45:55 +0900

    fix(clubBoardReply) : 로그인으로 인한 클럽 게시판 댓글 코드 수정 issue84

--- src/main/java/com/tennisPartner/tennisP/clubBoardReply/service/ClubBoardReplyServiceImpl.java
+++ src/main/java/com/tennisPartner/tennisP/clubBoardReply/service/ClubBoardReplyServiceImpl.java
@@ -1,6 +1,7 @@
 package com.tennisPartner.tennisP.clubBoardReply.service;
 
 import com.tennisPartner.tennisP.club.domain.Club;
+import com.tennisPartner.tennisP.club.domain.ClubJoin;
 import com.tennisPartner.tennisP.club.repository.ClubRepository;
 
 import com.tennisPartner.tennisP.clubBoard.domain.ClubBoard;
@@ -40,124 +41,122 @@ public class ClubBoardReplyServiceImpl implements ClubBoardReplyService {
     }
     @Override
     @Transactional
-    public ClubBoardReplyResponseDTO createClubBoardReply(Long clubIdx, Long clubBoardIdx, String replyContents) {
-        User writer = userRepository.findById(1L).get();
-
+    public ClubBoardReplyResponseDTO createClubBoardReply(Long clubIdx, Long clubBoardIdx, String replyContents, Long userIdx) {
+        Optional<User> findWriter = userRepository.findById(userIdx);
         Optional<Club> findClub = clubRepository.findById(clubIdx);
-
-        if(findClub.isEmpty() || findClub.get().getUseYn().equals("N")){
-            System.out.println("해당 클럽은 존재하지 않습니다.");
-            return null;
-        }
         Optional<ClubBoard> findClubBoard = clubBoardRepository.findById(clubBoardIdx);
 
-        if(findClubBoard.isEmpty() || findClubBoard.get().getUseYn().equals("N")){
-            System.out.println("해당 클럽 게시판은 존재하지 않습니다.");
+        if(findCheck(findClub, findWriter, findClubBoard)){
+            User writer = findWriter.get();
+            ClubBoardReply reply =  ClubBoardReply.builder()
+                .clubBoard(findClubBoard.get())
+                .writer(writer)
+                .replyContents(replyContents)
+                .build();
+
+            ClubBoardReply saveReply = clubBoardReplyRepository.save(reply);
+
+            ClubBoardReplyResponseDTO res = new ClubBoardReplyResponseDTO(saveReply);
+
+            return res;
+        }else{
             return null;
         }
 
-        ClubBoardReply reply =  ClubBoardReply.builder()
-            .clubBoard(findClubBoard.get())
-            .writer(writer)
-            .replyContents(replyContents)
-            .build();
-
-        ClubBoardReply saveReply = clubBoardReplyRepository.save(reply);
-
-        ClubBoardReplyResponseDTO res = new ClubBoardReplyResponseDTO(saveReply);
-
-        return res;
     }
 
     @Override
     @Transactional
-    public ClubBoardReplyResponseDTO updateClubBoardReply(Long clubIdx, Long boardIdx, Long clubBoardReplyIdx, String replyContents) {
-        User writer = userRepository.findById(1L).get();
-
+    public ClubBoardReplyResponseDTO updateClubBoardReply(Long clubIdx, Long clubBoardIdx, Long clubBoardReplyIdx, String replyContents, Long userIdx) {
+        Optional<User> findWriter = userRepository.findById(userIdx);
         Optional<Club> findClub = clubRepository.findById(clubIdx);
+        Optional<ClubBoard> findClubBoard = clubBoardRepository.findById(clubBoardIdx);
+        if(findCheck(findClub,findWriter,findClubBoard)){
+            Optional<ClubBoardReply> findClubBoardReply = clubBoardReplyRepository.findById(clubBoardReplyIdx);
+            if(findClubBoardReply.isEmpty()){
+                System.out.println("해당 댓글은 존재하지 않습니다.");
+                return null;
+            }
+            User writer = findWriter.get();
+            if(!writer.getUserIdx().equals(findClubBoardReply.get().getWriter().getUserIdx())){
+                System.out.println("작성자만 수정할 수 있습니다.");
+                return null;
+            }
 
-        if(findClub.isEmpty() || findClub.get().getUseYn().equals("N")){
-            System.out.println("해당 클럽은 존재하지 않습니다.");
+            findClubBoardReply.get().updateClubBoardReply(replyContents);
+
+            ClubBoardReplyResponseDTO res = new ClubBoardReplyResponseDTO(findClubBoardReply.get());
+
+            return res;
+        }else {
             return null;
         }
-        Optional<ClubBoard> findClubBoard = clubBoardRepository.findById(boardIdx);
 
-        if(findClubBoard.isEmpty() || findClubBoard.get().getUseYn().equals("N")){
-            System.out.println("해당 클럽 게시판은 존재하지 않습니다.");
-            return null;
-        }
-
-        Optional<ClubBoardReply> findClubBoardReply = clubBoardReplyRepository.findById(clubBoardReplyIdx);
-        if(findClubBoardReply.isEmpty()){
-            System.out.println("해당 댓글은 존재하지 않습니다.");
-            return null;
-        }
-        findClubBoardReply.get().updateClubBoardReply(replyContents);
-
-        ClubBoardReplyResponseDTO res = new ClubBoardReplyResponseDTO(findClubBoardReply.get());
-
-        return res;
     }
 
     @Override
     @Transactional
-    public void deleteClubBoardReply(Long clubIdx, Long clubBoardIdx, Long clubBoardReplyIdx) {
-        User writer = userRepository.findById(1L).get();
-
+    public void deleteClubBoardReply(Long clubIdx, Long clubBoardIdx, Long clubBoardReplyIdx, Long userIdx) {
+        Optional<User> findWriter = userRepository.findById(userIdx);
         Optional<Club> findClub = clubRepository.findById(clubIdx);
-
-        if(findClub.isEmpty() || findClub.get().getUseYn().equals("N")){
-            System.out.println("해당 클럽은 존재하지 않습니다.");
-            return;
-        }
         Optional<ClubBoard> findClubBoard = clubBoardRepository.findById(clubBoardIdx);
 
-        if(findClubBoard.isEmpty() || findClubBoard.get().getUseYn().equals("N")){
-            System.out.println("해당 클럽 게시판은 존재하지 않습니다.");
-            return;
-        }
+        if(findCheck(findClub,findWriter,findClubBoard)) {
+            User writer = findWriter.get();
+            Optional<ClubBoardReply> findClubBoardReply = clubBoardReplyRepository.findById(clubBoardReplyIdx);
+            if(findClubBoardReply.isEmpty()){
+                System.out.println("해당 댓글은 존재하지 않습니다.");
+                return;
+            }
+            System.out.println("writer : " + writer.getUserIdx() + "clubBoardReply : " + findClubBoardReply.get().getWriter().getUserIdx());
+            if(!writer.getUserIdx().equals(findClubBoardReply.get().getWriter().getUserIdx())){
+                System.out.println("작성자만 삭제할 수 있습니다.");
+                return;
+            }
 
-        Optional<ClubBoardReply> findClubBoardReply = clubBoardReplyRepository.findById(clubBoardReplyIdx);
-        if(findClubBoardReply.isEmpty()){
-            System.out.println("해당 댓글은 존재하지 않습니다.");
-            return;
+            clubBoardReplyRepository.deleteById(clubBoardReplyIdx);
         }
-        if(writer.getUserIdx().equals(findClubBoardReply.get().getWriter().getUserIdx())){
-            System.out.println("작성자만 삭제할 수 있습니다.");
-            return;
-        }
-
-        clubBoardReplyRepository.deleteById(clubBoardReplyIdx);
-
     }
 
     @Override
     @Transactional
     public Page<ClubBoardReplyResponseDTO> getClubBoardReplyList(Long clubIdx, Long clubBoardIdx, int page,
-        int size) {
+        int size, Long userIdx) {
 
         Optional<Club> findClub = clubRepository.findById(clubIdx);
-
-        if(findClub.isEmpty() || findClub.get().getUseYn().equals("N")){
-            System.out.println("해당 클럽은 존재하지 않습니다.");
-            return null;
-        }
+        Optional<User> findUser = userRepository.findById(userIdx);
         Optional<ClubBoard> findClubBoard = clubBoardRepository.findById(clubBoardIdx);
 
+        if(findCheck(findClub, findUser, findClubBoard)){
+            Pageable pageable = PageRequest.of(page, size, Sort.by("createDt"));
+            Page<ClubBoardReply> findList = clubBoardReplyRepository.findByClubBoard(findClubBoard.get(), pageable);
+
+            if(findList.isEmpty()){
+                System.out.println ("해당 요청에 대한 댓글 리스트가 존재하지 않습니다.");
+                return null;
+            }
+            Page<ClubBoardReplyResponseDTO> resList = findList.map(p -> new ClubBoardReplyResponseDTO(p));
+
+            return resList;
+        } else {
+            return null;
+        }
+    }
+
+    public boolean findCheck(Optional<Club> findClub, Optional<User> findUser, Optional<ClubBoard> findClubBoard ){
+        if(findClub.isEmpty() || findClub.get().getUseYn().equals("N")){
+            System.out.println("해당 클럽이 존재하지 않습니다.");
+            return false;
+        }
+        if(findUser.isEmpty() || findUser.get().getUseYn().equals("N")){
+            System.out.println("해당 유저가 존재하지 않습니다.");
+            return false;
+        }
         if(findClubBoard.isEmpty() || findClubBoard.get().getUseYn().equals("N")){
-            System.out.println("해당 클럽 게시판은 존재하지 않습니다.");
-            return null;
+            System.out.println("해당 클럽 게시판이 존재하지 않습니다.");
+            return false;
         }
 
-        Pageable pageable = PageRequest.of(page, size, Sort.by("createDt"));
-        Page<ClubBoardReply> findList = clubBoardReplyRepository.findByClubBoard(findClubBoard.get(), pageable);
-
-        if(findList.isEmpty()){
-            System.out.println ("해당 요청에 대한 클럽 리스트가 존재하지 않습니다.");
-            return null;
-        }
-        Page<ClubBoardReplyResponseDTO> resList = findList.map(p -> new ClubBoardReplyResponseDTO(p));
-
-        return resList;
+        return true;
     }
 }
