69f2beedf820b19e6246b40001de19f06c469b91
junho <yunjunhojj@gmail.com>
2023-04-27 20:36:25 +0900

    bug fix

--- src/front/src/routes.tsx
+++ src/front/src/routes.tsx
@@ -6,14 +6,14 @@ const MyPageIndex = React.lazy(() => import("./pages/myPage/index"));
 const AuthCreateProfile = React.lazy(
   () => import("./pages/auth/CreateProfile")
 );
-const AuthLogin = React.lazy(() => import("./pages/auth/Login"));
-const AuthSignup = React.lazy(() => import("./pages/auth/Signup"));
+const AuthLogin = React.lazy(() => import("./pages/auth/login"));
+const AuthSignup = React.lazy(() => import("./pages/auth/signup"));
 const BoardId = React.lazy(() => import("./pages/board/[id]"));
 const BoardBoardCreate = React.lazy(() => import("./pages/board/BoardCreate"));
 const BoardBoardPage = React.lazy(() => import("./pages/board/BoardPage"));
 const ClubDetail = React.lazy(() => import("./pages/club/[id]"));
 const ClubClubCreate = React.lazy(() => import("./pages/club/ClubCreate"));
-const ClubClubPage = React.lazy(() => import("./pages/club/ClubPage"));
+const ClubClubPage = React.lazy(() => import("./pages/club/clubPage"));
 const ClubClubBoardCreate = React.lazy(
   () => import("./pages/club/ClubBoardCreate")
 );
@@ -29,14 +29,14 @@ export const routes = [
       { path: "/auth/CreateProfile", element: <AuthCreateProfile /> },
       { path: "/auth/Login", element: <AuthLogin /> },
       { path: "/auth/Signup", element: <AuthSignup /> },
-      { path: "/board/id", element: <BoardId /> },
+      { path: "/board/:id", element: <BoardId /> },
       { path: "/board/BoardCreate", element: <BoardBoardCreate /> },
       { path: "/board/BoardPage", element: <BoardBoardPage /> },
-      { path: "/club/id", element: <ClubDetail /> },
+      { path: "/club/:id", element: <ClubDetail /> },
       { path: "/club/ClubCreate", element: <ClubClubCreate /> },
       { path: "/club/ClubPage", element: <ClubClubPage /> },
       { path: "/club/ClubBoardCreate", element: <ClubClubBoardCreate /> },
-      { path: "/club/board/id", element: <ClubBoardDetail /> },
+      { path: "/club/board/:id", element: <ClubBoardDetail /> },
     ],
   },
 ];
@@ -47,13 +47,13 @@ export const pages = [
   { route: "/auth/CreateProfile" },
   { route: "/auth/Login" },
   { route: "/auth/Signup" },
-  { route: "/board/id" },
+  { route: "/board/:id" },
   { route: "/board/BoardCreate" },
   { route: "/board/BoardPage" },
-  { route: "/club/id" },
+  { route: "/club/:id" },
   { route: "/club/ClubCreate" },
   { route: "/club/ClubPage" },
   { route: "/club/ClubBoardCreate" },
   { route: "/club/ClubBoardDetail" },
-  { route: "/club/board/id" },
+  { route: "/club/board/:id" },
 ];
