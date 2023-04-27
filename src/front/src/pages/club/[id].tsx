0003c0325fdfb6f4f6d173a85825941139567579
junho <yunjunhojj@gmail.com>
2023-04-27 15:25:55 +0900

    feat : delete club

--- src/front/src/pages/club/[id].tsx
+++ src/front/src/pages/club/[id].tsx
@@ -18,6 +18,9 @@ const ClubDetail = () => {
   const [clubBoardList, setClubBoardList] = useState<any>([]);
   const [targetState, setTargetState] = useState(false);
   const [totalPage, setTotalPage] = useState(100);
+  const [isChairman, setIsChairman] = useState(false);
+  const [userId, setUserId] = useState("");
+  const [isEdit, setIsEdit] = useState(false);
 
   const [clubInfo, setClubInfo] = useState({
     clubIdx: 0,
@@ -27,10 +30,31 @@ const ClubDetail = () => {
     clubCounty: "",
   });
 
+  const deleteClub = async () => {
+    const result = await axios
+      .patch(
+        `${baseUrl}/login/api/clubs/${clubIdx}`,
+        {
+          ...clubInfo,
+          useYn: "N",
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        }
+      )
+      .then((res) => {
+        navigate("/club/clubPage");
+      })
+      .catch((err) => {
+        console.log("club-err", err);
+      });
+  };
+
   // clubIdx로 클럽 정보 가져오기
   useEffect(() => {
     const clubIdx = id?.slice(1);
-
     const getData = async () => {
       // add clubIdx body data
       if (!clubIdx) {
@@ -44,6 +68,10 @@ const ClubDetail = () => {
           },
         })
         .then((res) => {
+          if (res.data.joinList[0].userDTO.userId === userId) {
+            setIsChairman(true);
+          }
+
           return res;
         })
         .catch((err) => {
@@ -55,9 +83,35 @@ const ClubDetail = () => {
     getData();
   }, []);
 
+  useEffect(() => {
+    const getUserInfo = async () => {
+      console.log("ge");
+      const result = await axios
+        .get(`${baseUrl}/login/api/users`, {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        })
+        .then((res) => {
+          console.log("res", res);
+          setUserId(res.data.userId);
+          return res;
+        })
+        .catch((err) => {
+          console.log("err", err);
+        });
+    };
+    getUserInfo();
+  }, []);
+
   const goToClubBoardCreate = () => {
     // 클럽 게시판 글쓰기 페이지로 이동
     navigate("/club/clubBoardCreate", { state: { clubIdx } });
+  };
+
+  const updateBoard = () => {
+    // isUpdate true로 변경
+    setIsEdit(true);
   };
 
   const fetchData = () => {
@@ -93,46 +147,92 @@ const ClubDetail = () => {
 
   return (
     <ClubDetailContainer>
-      <WriteButton onClick={goToClubBoardCreate}>글쓰기</WriteButton>
-      <ClubDetailWrapper>
-        <ClubProfileContainer>
-          <ClubDetailTitle>{clubInfo?.clubName}</ClubDetailTitle>
-          <ClubDetailContent>
-            클럽 지역 : {clubInfo?.clubCity}
-          </ClubDetailContent>
-          <ClubDetailContent>
-            상세 지역 : {clubInfo?.clubCounty}
-          </ClubDetailContent>
-          <ClubBoardContent> 소개 글 : {clubInfo?.clubInfo}</ClubBoardContent>
-        </ClubProfileContainer>
-        <ClubBoardContainer>
-          <ClubBoardTitle>게시판</ClubBoardTitle>
-          {/* 게시글 data map  */}
-          {
-            <BoardContainer>
-              {clubBoardList &&
-                clubBoardList?.map((board: any) => (
-                  <BoardPreview key={uuid()} board={board} />
-                ))}
-              {targetState && <div ref={ref}></div>}
-            </BoardContainer>
-          }
-        </ClubBoardContainer>
-      </ClubDetailWrapper>
+      {!isEdit ? (
+        <>
+          <ButtonContainer>
+            <WriteButton onClick={goToClubBoardCreate}>글쓰기</WriteButton>
+            {isChairman && (
+              <WriteButton onClick={deleteClub}>클럽 폐쇄</WriteButton>
+            )}
+          </ButtonContainer>
+          <ClubDetailWrapper>
+            <ClubProfileContainer>
+              <WriteButton style={{ marginLeft: "auto" }} onClick={updateBoard}>
+                수정하기
+              </WriteButton>
+              <ClubDetailTitle>{clubInfo?.clubName}</ClubDetailTitle>
+              <ClubDetailContent>
+                클럽 지역 : {clubInfo?.clubCity}
+              </ClubDetailContent>
+              <ClubDetailContent>
+                상세 지역 : {clubInfo?.clubCounty}
+              </ClubDetailContent>
+              <ClubBoardContent>
+                소개 글 : {clubInfo?.clubInfo}
+              </ClubBoardContent>
+            </ClubProfileContainer>
+            <ClubBoardContainer>
+              <ClubBoardTitle>게시판</ClubBoardTitle>
+              {/* 게시글 data map  */}
+              {
+                <BoardContainer>
+                  {clubBoardList &&
+                    clubBoardList?.map((board: any) => (
+                      <BoardPreview key={uuid()} board={board} />
+                    ))}
+                  {targetState && <div ref={ref}></div>}
+                </BoardContainer>
+              }
+            </ClubBoardContainer>
+          </ClubDetailWrapper>
+        </>
+      ) : (
+        <ClubDetailWrapper>
+          <ClubProfileContainer>
+            <ButtonContainer>
+              <WriteButton onClick={updateBoard}>수정 완료</WriteButton>
+              <WriteButton onClick={() => setIsEdit(false)}>
+                수정 취소
+              </WriteButton>
+            </ButtonContainer>
+          </ClubProfileContainer>
+        </ClubDetailWrapper>
+      )}
     </ClubDetailContainer>
   );
 };
 
-const WriteButton = styled.button`
+const Input = styled.input`
+  width: 100%;
+  height: 40px;
+  border-radius: 10px;
+  border: none;
+  background-color: ${({ theme }) => theme.colors.white};
+  font-size: 14px;
+  font-weight: 700;
+  color: ${({ theme }) => theme.colors.black};
+  cursor: pointer;
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  margin-bottom: 20px;
+
   position: absolute;
-  top: 30px;
-  right: 20px;
+  top: 40px;
+  right: 5%;
+
+  gap: 10px;
+`;
+
+const WriteButton = styled.button`
   width: 80px;
   height: 40px;
   border-radius: 10px;
   border: none;
   background-color: ${({ theme }) => theme.colors.primary};
-  font-size: 20px;
+  font-size: 14px;
   font-weight: 700;
   color: ${({ theme }) => theme.colors.white};
   cursor: pointer;
@@ -142,7 +242,6 @@ const ClubProfileContainer = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
-
   width: 250px;
   height: 300px;
 
@@ -221,6 +320,8 @@ const ClubBoardContainer = styled.div`
   align-items: center;
 
   margin-top: 20px;
+
+  width: 100%;
 `;
 
 const ClubBoardTitle = styled.div`
