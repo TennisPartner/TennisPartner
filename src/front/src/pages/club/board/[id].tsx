e2fe1109de81a8c86cf8b40507fce80ea504e50b
junho <yunjunhojj@gmail.com>
2023-05-14 21:47:18 +0900

    feat : comment CRUD

--- src/front/src/pages/club/board/[id].tsx
+++ src/front/src/pages/club/board/[id].tsx
@@ -1,20 +1,14 @@
 import axios from "axios";
+import instance from "../../../util/api";
 import { useEffect, useState } from "react";
 import { useParams, useLocation } from "react-router-dom";
 import styled from "styled-components";
 import Tag from "../../../components/Tag";
+import CommentComponent from "../../../components/Comment/CommentComponent";
 
 const ClubBoardDetail = () => {
   const baseUrl = import.meta.env.VITE_APP_BACK_END_AWS;
   const accessToken = localStorage.getItem("accessToken");
-
-  const { id } = useParams<{ id: string }>();
-  const { state } = useLocation();
-
-  const [isWanted, setIsWanted] = useState(false);
-  const [isEdit, setIsEdit] = useState(false);
-  const [isFullMember, setIsFullMember] = useState(false);
-  const [isWriter, setIsWriter] = useState(false);
 
   const [board, setBoard] = useState({
     clubBoardIdx: 0,
@@ -29,12 +23,126 @@ const ClubBoardDetail = () => {
     joinList: [],
   } as any);
 
+  const { id } = useParams<{ id: string }>();
+  const { state } = useLocation();
+
+  const [isWanted, setIsWanted] = useState(false);
+  const [isEdit, setIsEdit] = useState(false);
+  const [isFullMember, setIsFullMember] = useState(false);
+  const [isWriter, setIsWriter] = useState(false);
+
+  const [comment, setComment] = useState("");
+  const [commentList, setCommentList] = useState([] as any);
+  const [editComment, setEditComment] = useState("");
+
+  const addComment = () => {
+    // request body 에 comment를 raw로 넣어서 보내기
+    const response = instance({
+      method: "post",
+      url: `${baseUrl}/login/api/clubs/${clubIdx}/boards/${clubBoardIdx}/replys`,
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        "Content-Type": "application/json",
+      },
+      data: comment,
+    });
+    response
+      .then((res) => {
+        console.log("res.datares.data", res.data);
+        setCommentList([...commentList, res.data]);
+        setComment("");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    console.log(response);
+  };
+
+  // 댓글 삭제
+  const deleteComment = (clubBoardReplyIdx: number) => {
+    const response = instance.delete(
+      `${baseUrl}/login/api/clubs/${clubIdx}/boards/${clubBoardIdx}/replys/${clubBoardReplyIdx}`,
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      }
+    );
+    response
+      .then((res) => {
+        console.log("comment", res);
+        setCommentList(
+          commentList.filter(
+            (comment: any) => comment.clubBoardReplyIdx !== clubBoardReplyIdx
+          )
+        );
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    console.log(response);
+  };
+
+  // 댓글 수정
+  const updateComment = (clubBoardReplyIdx: number) => {
+    const response = instance({
+      method: "put",
+      url: `${baseUrl}/login/api/clubs/${clubIdx}/boards/${clubBoardIdx}/replys/${clubBoardReplyIdx}`,
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        "Content-Type": "application/json",
+      },
+      data: editComment,
+    });
+    response
+      .then((res) => {
+        console.log(res);
+        setCommentList(
+          commentList.map((comment: any) => {
+            if (comment.clubBoardReplyIdx === clubBoardReplyIdx) {
+              return { ...comment, replyContents: editComment };
+            }
+            return comment;
+          })
+        );
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    console.log(response);
+  };
+
+  // 댓글 불러오기
+  const getComment = () => {
+    const response = instance.get(
+      `${baseUrl}/login/api/clubs/${clubIdx}/boards/${clubBoardIdx}/replys`,
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      }
+    );
+    response
+      .then((res) => {
+        console.log("comment", res.data.content);
+        setCommentList(res.data.content);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    console.log(response);
+  };
+
   const clubIdx = state?.clubIdx;
   const clubBoardIdx = id;
   const userId = state?.userId;
 
   const cancelBoard = () => {
-    const response = axios.delete(
+    const response = instance.delete(
       `${baseUrl}/login/api/clubs/${clubIdx}/boards/${clubBoardIdx}/join`,
       {
         headers: {
@@ -62,7 +170,7 @@ const ClubBoardDetail = () => {
   };
 
   const joinBoard = () => {
-    const response = axios.post(
+    const response = instance.post(
       `${baseUrl}/login/api/clubs/${clubIdx}/boards/${clubBoardIdx}/join`,
       {
         userId,
@@ -87,7 +195,7 @@ const ClubBoardDetail = () => {
   };
 
   const editBoard = () => {
-    const response = axios.patch(
+    const response = instance.patch(
       `${baseUrl}/login/api/clubs/${clubIdx}/boards/${clubBoardIdx}`,
       {
         ...board,
@@ -110,8 +218,8 @@ const ClubBoardDetail = () => {
     console.log(response);
   };
 
-  useEffect(() => {
-    const response = axios.get(
+  const deleteBoard = () => {
+    const response = instance.delete(
       `${baseUrl}/login/api/clubs/${clubIdx}/boards/${clubBoardIdx}`,
       {
         headers: {
@@ -119,6 +227,27 @@ const ClubBoardDetail = () => {
         },
       }
     );
+    response
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    console.log(response);
+  };
+
+  useEffect(() => {
+    const response = instance.get(
+      `${baseUrl}/login/api/clubs/${clubIdx}/boards/${clubBoardIdx}`,
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      }
+    );
+
     response
       .then((res) => {
         // 2023-04-30T17:22:00 -> 2023-04-30 17:22:00 으로 변환
@@ -139,12 +268,19 @@ const ClubBoardDetail = () => {
         if (res.data.writerDTO.userId === userId) {
           setIsWriter(true);
         }
+        console.log("res.data.status", res.data.status);
       })
       .catch((err) => {
         console.log(err);
       });
   }, []);
 
+  // 댓글 불러오기
+  useEffect(() => {
+    getComment();
+  }, []);
+
+  console.log("userIduserIduserIds", userId);
   return (
     <BoardContainer>
       <BoardCard>
@@ -189,7 +325,9 @@ const ClubBoardDetail = () => {
         ) : (
           <StyledTitle>{board?.clubBoardTitle}</StyledTitle>
         )}
-        <StyledWriter>작성자 : {board?.writerDTO?.userNickName}</StyledWriter>
+        <StyledWriter>
+          작성자 : {board?.writerDTO?.userNickname || board?.writerDTO?.userId}
+        </StyledWriter>
         {isEdit ? (
           <EditBox>
             <label>내용 : </label>
@@ -208,7 +346,6 @@ const ClubBoardDetail = () => {
         {board?.clubBoardType === "M" && (
           <>
             <StyledDate>
-              {" "}
               만나는 날 : {board?.meetDt.replace("T", ",  시간 :")}
             </StyledDate>
             <StyledJoinList>
@@ -226,6 +363,30 @@ const ClubBoardDetail = () => {
           </>
         )}
       </BoardCard>
+      <CommentContainer>
+        <CommentBox>
+          <CommentInput
+            type="text"
+            value={comment}
+            onChange={(e) => setComment(e.target.value)}
+            placeholder="댓글을 입력하세요"
+          />
+          <CommentButton onClick={() => addComment()}>등록</CommentButton>
+        </CommentBox>
+        <CommentList>
+          {commentList?.map((comment: any) => (
+            <CommentComponent
+              key={comment.clubBoardCommentIdx}
+              comment={comment}
+              deleteComment={deleteComment}
+              updateComment={updateComment}
+              editComment={editComment}
+              setEditComment={setEditComment}
+              userId={userId}
+            />
+          ))}
+        </CommentList>
+      </CommentContainer>
     </BoardContainer>
   );
 };
@@ -234,7 +395,7 @@ const BoardContainer = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  height: 100vh;
+  height: calc(100vh - 50px);
   background-color: ${({ theme }) => theme.colors.tennis};
 `;
 
@@ -270,11 +431,6 @@ const Input = styled.input`
 const ButtonContainer = styled.div`
   display: flex;
   justify-content: flex-end;
-  margin-bottom: 20px;
-
-  position: absolute;
-  bottom: 40px;
-  right: 5%;
 
   gap: 10px;
 `;
@@ -328,6 +484,53 @@ const StyledJoinList = styled.div`
 const TagName = styled.div`
   font-weight: bold;
   min-width: 60px;
+`;
+
+const CommentContainer = styled.div`
+  width: 90%;
+  height: 80%;
+  background-color: ${({ theme }) => theme.colors.white};
+  border-radius: 5px;
+  padding: 20px;
+  font-size: 16px;
+  line-height: 1.5;
+  text-align: justify;
+
+  overflow-y: scroll;
+`;
+
+const CommentBox = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+`;
+
+const CommentInput = styled.input`
+  width: 80%;
+  height: 32px;
+  border: 1px solid ${({ theme }) => theme.colors.black};
+  border-radius: 5px;
+`;
+
+const CommentButton = styled.button`
+  width: 80px;
+  height: 32px;
+  background-color: ${({ theme }) => theme.colors.gray};
+  border: 1px solid ${({ theme }) => theme.colors.black};
+  border-radius: 5px;
+  font-weight: bold;
+`;
+
+const CommentList = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  overflow-y: scroll;
+
+  &::-webkit-scrollbar {
+    display: none;
+  }
 `;
 
 export default ClubBoardDetail;
